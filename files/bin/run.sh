f1e617f1d50622cb305233e696c5d6644d8e9ba6
gaowenbo <gaowenbo_xa@netposa.com>
2020-11-04 23:30:50 +0800

    [*]1.调整安装包

--- files/bin/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export LD_LIBRARY_PATH=/opt/apps/com.deepin.youboxclient/files/bin
-/opt/apps/com.deepin.youboxclient/files/bin/youboxclient
---
+++ files/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export LD_LIBRARY_PATH=/opt/apps/com.deepin.youboxclient/files
+/opt/apps/com.deepin.youboxclient/files/youboxclient
