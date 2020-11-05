b64abafe9f2a1f3d4b1cd2866638de404bf9b340
gaowenbo <gaowenbo_xa@netposa.com>
2020-11-05 21:09:28 +0800

    [*]1.修改打包信息

--- files/run.sh
+++ files/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=/opt/apps/com.deepin.youboxclient/files
-/opt/apps/com.deepin.youboxclient/files/youboxclient
+export LD_LIBRARY_PATH=/opt/apps/com.whsrc.youboxclient/files
+/opt/apps/com.whsrc.youboxclient/files/youboxclient
