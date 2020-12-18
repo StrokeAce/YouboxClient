88cd469247a771b9592dc108cf0198a2d0643721
gaowenbo <gaowenbo_xa@netposa.com>
2020-12-18 15:05:53 +0800

    [*]1.麒麟的界面差异化代码保存

---
+++ kelin/RdpRemoteTools_zh_CN.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="RdpRemoteTools_zh_CN">
+<context>
+    <name>Agreement</name>
+    <message>
+        <location filename="agreement.ui" line="14"/>
+        <location filename="build/ui_agreement.h" line="61"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="74"/>
+        <location filename="build/ui_mainwindow.h" line="199"/>
+        <source>用户名/账号</source>
+        <oldsource>  用户名/账号</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="140"/>
+        <location filename="build/ui_mainwindow.h" line="201"/>
+        <source>密码</source>
+        <oldsource>  密码</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MessageBox</name>
+    <message>
+        <location filename="messagebox.ui" line="14"/>
+        <location filename="build/ui_messagebox.h" line="74"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
